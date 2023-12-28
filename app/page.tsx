b975f6e1841e6aa79a616717bd0e9cb20411fbcf
hawalinajwan <muhammadhawalinajwan@gmail.com>
2023-12-28 14:53:04 +0700

    pp

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,10 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center  bg-gray-950">
 
-      <div className="z-10 max-w-5xl space-x-16 items-center justify-between font-mono lg:grid-cols-2 text-sm lg:flex">
+      <div className="z-10 max-w-5xl space-x-16 items-center justify-between font-mono lg:grid-cols-2 text-sm lg:flex lg:inset-0">
 
         <Image
-          className="relative"
+          className="relative scroll-smooth lg:inset-0 lg:object-center"
           src="/images/Ak (2).png"
           alt="kxm Logo"
           width={400}
@@ -17,21 +17,22 @@ export default function Home() {
 
         <a
           href="https://discord.gg/YfZjqSsVz8"
-          className="group rounded-lg border border-transparent py8  transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="group rounded-lg border border-transparent py-5 px-2  transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer">
 
-
-          <a className={`m-1 text-base opacity-90 `}>
-            Discord Server
-          </a>
+          <p>
+            <a className={`m-1 text-base opacity-90 `}>
+              Discord Server
+            </a>
+          </p>
           <p>
             <h2 className={`mb-5 text-8xl font-semibold text-white`}>
               <a className={`text-red-300`}>K</a>ELAS <a className={`text-red-900`} >A</a>XEM
             </h2>
           </p>
-          <p className={`m-1 text-lg opacity-50 text-white`}>
-            Click to Join
+          <p className={`m-1   text-lg opacity-60 text-white animate-bounce tracking-wide`}>
+            Click to Join 
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
