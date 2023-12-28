44a0562e650539dd6cba876deb318b75d7870419
hawalinajwan <muhammadhawalinajwan@gmail.com>
2023-12-28 22:21:50 +0700

    pppp

--- app/page.tsx
+++ app/page.tsx
@@ -2,22 +2,23 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center  bg-gray-950">
+    <main className="flex min-h-screen flex-col items-center justify-center  bg-neutral-900 transition ">
 
-      <div className="z-10 max-w-5xl space-x-16 items-center justify-between font-mono lg:grid-cols-2 text-sm lg:flex lg:inset-0">
-
-        <Image
-          className="relative scroll-smooth lg:inset-0 lg:object-center"
-          src="/images/Ak (2).png"
-          alt="kxm Logo"
-          width={400}
-          height={400}
-          priority
-        />
+      <div className="z-10 max-w-5xl  items-center justify-between font-mono lg:grid-cols-2 text-sm lg:flex lg:inset-0">
+        <a>
+          <Image
+            className="relative scroll-smooth lg:inset-0 md:mx-auto"
+            src="/images/Ak (2).png"
+            alt="kxm Logo"
+            width={400}
+            height={400}
+            priority
+          />
+        </a>
 
         <a
           href="https://discord.gg/YfZjqSsVz8"
-          className="group rounded-lg border border-transparent py-5 px-2  transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="group rounded-lg border border-transparent py-4 px-2  transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer">
 
@@ -28,11 +29,11 @@ export default function Home() {
           </p>
           <p>
             <h2 className={`mb-5 text-8xl font-semibold text-white`}>
-              <a className={`text-red-300`}>K</a>ELAS <a className={`text-red-900`} >A</a>XEM
+              <a className={`text-red-300`}>K</a>ELAS<a className={`text-red-900`} > A</a>XEM
             </h2>
           </p>
           <p className={`m-1   text-lg opacity-60 text-white animate-bounce tracking-wide`}>
-            Click to Join 
+            Click to Join
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
